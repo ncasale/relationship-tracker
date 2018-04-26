7edb264de486ecdf09ae2678b6a134dfe4285805
Nate Casale <casalenm924@gmail.com>
2018-04-26 17:49:45 -0400

    Added snackbar call when leaving relationship

--- assets/app/dashboard/mydash/settings/leave-dialog.component.ts
+++ assets/app/dashboard/mydash/settings/leave-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, Input } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from "@angular/material";
 import { FormControl, Validators } from "@angular/forms";
 import { RelationshipService } from "../../../relationships/relationship.service";
 import { Router } from "@angular/router";
@@ -19,13 +19,25 @@ export class LeaveDialogComponent {
         @Inject(MAT_DIALOG_DATA) public data: any,
         private relationshipService: RelationshipService,
         private router: Router,
-        private myDashService: MyDashService
+        private myDashService: MyDashService,
+        public snackBar: MatSnackBar
     ) {}
 
+    /**
+     * Does a string compare of new password and confirm password
+     * 
+     * @returns true if confirm matches new, false otherwise
+     * @memberof LeaveDialogComponent
+     */
     compareName() {
         return this.nameConfirmationFC.value == this.data.relationship.title;
     }
 
+    /**
+     * Call relationship service to leave this relationship. Route user to dashboard
+     * 
+     * @memberof LeaveDialogComponent
+     */
     leaveRelationship() {
         this.dialogRef.close();
         this.relationshipService.leaveRelationship(this.data.relationship.relationshipId)
@@ -35,5 +47,7 @@ export class LeaveDialogComponent {
                     this.router.navigateByUrl('/dashboard');
                 }
             )
+        //Open snackbar alert
+        this.myDashService.openSnackBar('Left Relationship', 'close');
     }
 }
