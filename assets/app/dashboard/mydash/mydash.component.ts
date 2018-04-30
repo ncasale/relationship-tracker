b7ed4d6b51f158dd6e633791f9d27a89fb25b696
Nate Casale <casalenm924@gmail.com>
2018-04-30 11:15:49 -0400

    Fixed null check for token in workaround

--- assets/app/dashboard/mydash/mydash.component.ts
+++ assets/app/dashboard/mydash/mydash.component.ts
@@ -34,7 +34,7 @@ export class MyDashComponent {
         console.log('Got to MyDash ngoninit');
         console.log('Token: ', localStorage.getItem('token'));
         //Workaround to redirect to home if we accidentally get here without logging in
-        if(localStorage.getItem('token')) {
+        if(!localStorage.getItem('token')) {
             console.log('No token, back to auth');
             this.router.navigateByUrl('/auth/login');
         } 
