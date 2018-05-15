ebe42ffb2ae41a5d098acdedb44a2bd6e8d547a7
Nate Casale <casalenm924@gmail.com>
2018-05-15 15:02:45 -0400

    Added authorization check for fights

--- routes/fight.js
+++ routes/fight.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+var utils = require('./utils');
 
 var Fight = require('../models/fight');
+var Relationship = require('../models/relationship');
 
 /**
  * Route to save new fight to database
@@ -10,9 +12,6 @@ var Fight = require('../models/fight');
 router.post('/add', function(req, res, next) {
     //Decode token
     var decoded = jwt.decode(req.query.token);
-
-    console.log("Fight body: ", req.body);
-
     //Create fight
     var fight = new Fight({
         title: req.body.title,
@@ -23,19 +22,33 @@ router.post('/add', function(req, res, next) {
         relationshipId: req.body.relationshipId,
         createUserId: decoded.user._id
     });
-    //Save fight
-    fight.save(function(err, savedFight) {
-        if(err) {
-            return res.status(500).json({
-                title: 'An error occurred',
-                error: err
+    //See if user authorized to save fight to this relationship
+    fight.populate('relationshipId', function(err) {
+        //Function to call if user authorized
+        function saveFight() {
+            fight.save(function(err, savedFight) {
+                if(err) {
+                    return res.status(500).json({
+                        title: 'An error occurred',
+                        error: err
+                    })
+                }
+                //Successfully saved
+                return res.status(201).json({
+                    title: 'Fight saved',
+                    obj: savedFight
+                })
             })
         }
-        //Successfully saved
-        return res.status(201).json({
-            title: 'Fight saved',
-            obj: savedFight
-        })
+        //Function to call if user unauthorized
+        function unAuth() {
+            return res.status(401).json({
+                title: 'Unauthorized',
+                error: {message: 'Unauthorized'}
+            })
+        }
+        //Check authorization
+        utils.checkRelationship(decoded, fight.relationshipId.users, saveFight, unAuth);
     })
 })
 
@@ -45,19 +58,45 @@ router.post('/add', function(req, res, next) {
 router.post('/getfights/:relationshipId', function(req, res, next) {
     //Decode token
     var decoded = jwt.decode(req.query.token);
-    //Find all fights with relationshipId
-    Fight.find({relationshipId: req.params.relationshipId}, function(err, fights) {
+    //Check that user is a member of relationship
+    Relationship.findById(req.params.relationshipId, function(err, relationship) {
         if(err) {
             return res.status(500).json({
-                title: 'An error occurred', 
+                title: 'An error occurred',
                 error: err
             })
         }
-        //Found fights
-        return res.status(200).json({
-            title: 'Fights found',
-            obj: fights
-        })
+        if(!relationship) {
+            return res.status(404).json({
+                title: 'Relationship not found',
+                error: {message: 'Relationship not found'}
+            })
+        }
+        //Function to call if user authorized to get these fights
+        function getFights() {
+            Fight.find({relationshipId: req.params.relationshipId}, function(err, fights) {
+                if(err) {
+                    return res.status(500).json({
+                        title: 'An error occurred', 
+                        error: err
+                    })
+                }
+                //Found fights
+                return res.status(200).json({
+                    title: 'Fights found',
+                    obj: fights
+                })
+            })
+        }
+        //Function to call if user unauthorized
+        function unAuth() {
+            return res.status(401).json({
+                title: 'Unauthorized',
+                error: {message: 'Unauthorized'}
+            })
+        }
+        //Check authorization
+        utils.checkRelationship(decoded, relationship.users, getFights, unAuth);
     })
 })
 
@@ -81,30 +120,43 @@ router.patch('/edit', function(req, res, next) {
                 error: {message: 'Fight not found'}
             })
         }
-        //TODO: Add checking to see that user is member of relationship
-        
-        //Edit fight
-        fight.title = req.body.title;
-        fight.descriptions = req.body.descriptions;
-        fight.causes = req.body.causes;
-        fight.resolutions = req.body.resolutions;
-        fight.fightDate = req.body.fightDate;
-        fight.editUserId = decoded.user._id;
-        fight.editTimestamp = Date.now();
-        //Save fight
-        fight.save(function(err, savedFight) {
-            if(err) {
-                return res.status(500).json({
-                    title: 'An error occurred',
-                    error: err
+        //Check if user authorized to edit fight
+        fight.populate('relationshipId', function(err) {
+            //Function to call if user is authorized
+            function editFight() {
+                //Edit fight
+                fight.title = req.body.title;
+                fight.descriptions = req.body.descriptions;
+                fight.causes = req.body.causes;
+                fight.resolutions = req.body.resolutions;
+                fight.fightDate = req.body.fightDate;
+                fight.editUserId = decoded.user._id;
+                fight.editTimestamp = Date.now();
+                //Save fight
+                fight.save(function(err, savedFight) {
+                    if(err) {
+                        return res.status(500).json({
+                            title: 'An error occurred',
+                            error: err
+                        })
+                    }
+                    //Successfully saved
+                    return res.status(201).json({
+                        title: 'Fight Edited',
+                        obj: savedFight
+                    })
                 })
             }
-            //Successfully saved
-            return res.status(201).json({
-                title: 'Fight Edited',
-                obj: savedFight
-            })
-        })
+            //Function to call if user unauthorized
+            function unAuth() {
+                return res.status(401).json({
+                    title: 'Unauthorized',
+                    error: {message: 'Unauthorized'}
+                })
+            }
+            //Check authorization
+            utils.checkRelationship(decoded, fight.relationshipId.users, editFight, unAuth);
+        })        
     })
 })
 
@@ -128,21 +180,35 @@ router.delete('/delete/:fightId', function(req, res, next) {
                 error: {message: 'Fight not found'}
             })
         }
-        //Remove fight
-        fight.remove(function(err, deletedFight) {
-            if(err) {
-                return res.status(500).json({
-                    title: 'An error occurred',
-                    error: err
+        //Check if user authorized to remove fight
+        fight.populate('relationshipId', function(err) {
+            //Function to call if user authorized
+            function deleteFight() {
+                fight.remove(function(err, deletedFight) {
+                    if(err) {
+                        return res.status(500).json({
+                            title: 'An error occurred',
+                            error: err
+                        })
+                    }
+                    
+                    //Successfully deleted
+                    return res.status(200).json({
+                        title: 'Fight deleted',
+                        obj: deletedFight
+                    })
+                    
                 })
             }
-            
-            //Successfully deleted
-            return res.status(200).json({
-                title: 'Fight deleted',
-                obj: deletedFight
-            })
-            
+            //Function to call if user unauthorized
+            function unAuth() {
+                return res.status(401).json({
+                    title: 'Unauthorized',
+                    error: {message: 'Unauthorized'}
+                })
+            }
+            //Check authorization
+            utils.checkRelationship(decoded, fight.relationshipId.users, deleteFight, unAuth);
         })
     })
 })
@@ -167,12 +233,25 @@ router.post('/getFight/:fightId', function(req, res, next) {
                 error: {message: 'Fight not found'}
             })
         }
-        //Found fight
-        return res.status(200).json({
-            title: 'Fight found',
-            obj: fight
+        //Check if user is authorized to get this fight
+        fight.populate('relationshipId', function(err) {
+            //Function to call if user is authorized
+            function getFight() {
+                return res.status(200).json({
+                    title: 'Fight found',
+                    obj: fight
+                })
+            }
+            //Function to call if user unauthorized
+            function unAuth() {
+                return res.status(401).json({
+                    title: 'Unauthorized',
+                    error: {message: 'Unauthorized'}
+                })
+            }
+            //Check authorization
+            utils.checkRelationship(decoded, fight.relationshipId.users, getFight, unAuth);
         })
-
     })
 })
 
